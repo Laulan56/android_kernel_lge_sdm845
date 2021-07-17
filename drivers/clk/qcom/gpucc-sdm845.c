94c56b92b07b34ca38c71a21820d536cc1880486
Stephen Xian <stevexian56@gmail.com>
2021-07-17 20:59:21 +0800

    Revert "GPUFreq: adjust GPU freq"
    This reverts commit 6f39d3ab2646cc88474bfb359abfb099c2d655ce.

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -53,8 +53,6 @@ static int vdd_gx_corner[] = {
 	RPMH_REGULATOR_LEVEL_NOM_L1,		/* VDD_GX_NOMINAL_L1 */
 	RPMH_REGULATOR_LEVEL_TURBO,		/* VDD_GX_HIGH */
 	RPMH_REGULATOR_LEVEL_TURBO_L1,		/* VDD_GX_HIGH_L1 */
-	RPMH_REGULATOR_LEVEL_TURBO_L1,		/* VDD_GX_HIGH_L2 */
-	RPMH_REGULATOR_LEVEL_NOM_L1,		/* VDD_GX_NOMINAL_L2 */
 	RPMH_REGULATOR_LEVEL_MAX,		/* VDD_GX_MAX */
 };
 
@@ -277,14 +275,13 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
 
 static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(180000000, P_CRC_DIV,  1, 0, 0),
-	F(380000000, P_CRC_DIV,  1, 0, 0),
-	F(460000000, P_CRC_DIV,  1, 0, 0),
-	F(530000000, P_CRC_DIV,  1, 0, 0),
-	F(580000000, P_CRC_DIV,  1, 0, 0),
+	F(257000000, P_CRC_DIV,  1, 0, 0),
+	F(342000000, P_CRC_DIV,  1, 0, 0),
+	F(414000000, P_CRC_DIV,  1, 0, 0),
+	F(520000000, P_CRC_DIV,  1, 0, 0),
+	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
-	F(720000000, P_CRC_DIV,  1, 0, 0),
-	F(760000000, P_CRC_DIV,  1, 0, 0),
-	F(820000000, P_CRC_DIV,  1, 0, 0),
+	F(710000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -320,13 +317,11 @@ static struct clk_rcg2 gpu_cc_gx_gfx3d_clk_src = {
 			MIN, 147000000,
 			LOWER, 210000000,
 			LOW, 280000000,
-			LOW_L1, 350000000,
-			NOMINAL, 427000000,
-			NOMINAL_L1, 480000000,
+			LOW_L1, 338000000,
+			NOMINAL, 425000000,
+			NOMINAL_L1, 487000000,
 			HIGH, 548000000,
-			HIGH_L1, 580000000,
-			HIGH_L2, 600000000,
-			NOMINAL_L2, 500000000),
+			HIGH_L1, 600000000),
 	},
 };
 
@@ -633,20 +628,18 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.freq_tbl =
 				ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MIN] = 180000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] =
-		380000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] = 460000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOWER] =
+		257000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 342000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] =
+		414000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
-		530000000;
+		520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		580000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L2] = 675000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] =
-		720000000;
+		596000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		760000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L2] =
-		820000000;
+		710000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
